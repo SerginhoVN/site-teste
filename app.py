0b44c7859a4a17359b1cef99fa9e59c7eeb5c12c
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-03-13 22:11:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ menu = """ #tem três aspas pq é string multilinhas
 
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é meu site. (Sérgio Vieira)"
+  return menu + "Olá, mundo! Esse é meu site. (Sérgio Vieira)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
