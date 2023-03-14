4b6c6babea44333a41913567a0dc06aad036a8fd
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-03-13 22:37:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = """
-<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> ! <a href="/contato">Contato</a>
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
-def hello_world():
-  return menu + "Olá, mundo! Esse é o meu site. (Sérgio Vieira)"
-
+def index():
+  return menu + "Olá, mundo! Esse é meu site. (Álvaro Justen)"
 @app.route("/sobre")
 def sobre():
   return menu + "Aqui vai o conteúdo da página Sobre"
-
 @app.route("/contato")
 def contato():
-  return menu + "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
