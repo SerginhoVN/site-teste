801971a94fc055a359c5942b396f4ac93f2dd1ef
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-03-13 21:51:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Sérgio Vieira)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página Sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da página Contato"
