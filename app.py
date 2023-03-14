1d8a2cfc587e938968e863f58ad928e110b052f1
Sérgio Vieira <114782290+SerginhoVN@users.noreply.github.com>
2023-03-13 22:20:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 menu = """ 
-< a href="/">Página Inicial</a> | < a href="/sobre">Sobre</a> | < a href="/contato">Contato</a><br>
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a><br>
 """
 
 @app.route("/")
